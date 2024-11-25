7bc2cb12a84ccbc4ca8247752ff6c7d13e9950b1
EnesNI <enesnimani87@gmail.com>
2024-11-25 17:48:36 +0100

    as

--- moddule9/post_nethod/db.php
+++ moddule9/post_nethod/db.php
@@ -5,11 +5,13 @@ $username = "root";
 $password = "";
 $dbname = "users";
 
-
-try {
-    $connect = new PDO('mysql:host=localhost;dbname=your_database', 'your_username', 'your_password');
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+try{
+    $connect = new PDO("mysql:host=$server;dbname=$dbnanme", $username, $password);
 }
+
+catch(PDOException $e){
+    echo "Something went wrong!!";
+}
+
+
 ?>
