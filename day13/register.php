59a02fae8d08bcb21f7ab05ae0a2c43dbb75a37d
EnesNI <enesnimani87@gmail.com>
2024-12-18 17:44:25 +0100

    DonikaPllana

--- day13/register.php
+++ day13/register.php
@@ -1,5 +1,38 @@
+<?php 
+require_once("config.php");
+$error="";
+$success="";
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $username=mysqli_real_escape_string($conn,$_POST['username']);
+    $email=mysqli_real_escape_string($conn,$_POST['email']);
+    $password=mysqli_real_escape_string($conn,$_POST['password']);
+}
+
+      if(empty($username)|| empty($email) || empty($password)){
+        $error="All fields are required";
+      }else{
+        $check_user="SELECT * FROM users WHERE username='$username' OR email='$email'";
+        $result=$conn->query($check_user);
+
+        if($result->num_rows>0){
+            $error="Username or email already exists";
+        }else{
+            $hashed_password=md5($password);
+            $sql="INSERT INTO users(username,email,password) VALUES ('$username','$email','$hashed_password')";
+            if($conn->query($sql)===True){
+                $success="REgistration successful! You can now <a herf='login.php'>Login here </a>";
+            }else{
+                $error="Error:" . $conn->error;
+            }
+        }
+      }
+?>
+
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
